d5c26afa54c93c68437d0e53ca059c57d51ca556
FBiDev <68982125+FBiDev@users.noreply.github.com>
2022-03-17 20:15:09 -0300

    Fixed Address

--- RADB/src/cls/Config.cs
+++ RADB/src/cls/Config.cs
@@ -11,7 +11,7 @@ namespace RADB
         public static string AuthQS = "?z=FBiDev&y=uBuG840fXTyKSQvS8MFKX5d40fOelJ29";
         public static WebProxy Proxy = new WebProxy
         {
-            Address = "http://cohab-proxy.cohabct.com.br:3128",
+            Address = new Uri("http://cohab-proxy.cohabct.com.br:3128"),
             BypassProxyOnLocal = true,
             BypassList = new string[] { },
             Credentials = new NetworkCredential("fbirnfeld", "zumbie")
