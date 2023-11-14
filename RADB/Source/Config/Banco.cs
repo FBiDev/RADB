9f61990ef1962a63a3804b1a7c33106b8b532e29
FBiDev <fernando.birnfeld@gmail.com>
2023-11-14 17:57:02 -0300

    Fix: GNX dll

--- RADB/Source/Config/Banco.cs
+++ RADB/Source/Config/Banco.cs
@@ -10,13 +10,13 @@ namespace RADB
 {
     public static class Banco
     {
-        static DataBaseManager Database { get; set; }
+        static DatabaseManager Database { get; set; }
         public static ListSynced<cLogSQL> Log { get { return Database.Log; } }
         public static bool Loaded { get; set; }
 
         public static void Load()
         {
-            Database = new DataBaseManager
+            Database = new DatabaseManager
             {
                 DatabaseSystem = DbSystem.SQLite,
                 Connection = new SQLiteConnection(),
@@ -49,16 +49,6 @@ namespace RADB
                 catch (Exception ex) { ExceptionManager.Resolve(ex); }
             }
             return new cSqlResult();
-        }
-
-        public async static Task<int> GetLastID()
-        {
-            if (Loaded)
-            {
-                try { return await Database.GetLastID(); }
-                catch (Exception ex) { ExceptionManager.Resolve(ex); }
-            }
-            return 0;
         }
 
         public async static Task<DateTime> DataServidor()
