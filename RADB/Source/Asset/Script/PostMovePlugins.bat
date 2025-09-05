5fbff77751c6c552ca424b244a898124c0d11994
FBiDev <fernando.birnfeld@gmail.com>
2025-09-05 17:10:37 -0300

    Fix: SQLite

--- RADB/Source/Asset/Script/PostMovePlugins.bat
+++ RADB/Source/Asset/Script/PostMovePlugins.bat
@@ -6,11 +6,26 @@ set "EventName=Move Plugins"
 
 set "ProjectName=%1"
 set "TargetDir=%2"
+set "SolutionDir=%3"
+set "PlatformName=%4"
+set "ProjectFileName=%5"
+
+set "Platform=x64"
 
 echo ===============================================================================
 echo    Project      ::     %ProjectName%
 echo    Event        ::     Begin %EventType% -^> %EventName%
 echo ===============================================================================
+
+if /I %PlatformName%==AnyCPU (
+	for /F "delims=" %%i in ('type "%ProjectFileName%" ^| findstr /R /C:"<Prefer32Bit>true</Prefer32Bit>"') do (
+		set Platform=x86
+	)
+) else if /I %PlatformName%==x86 (
+	set Platform=x86
+)
+
+robocopy "%SolutionDir%..\App\Bin\%Platform%\ " "%TargetDir%Plugins\%Platform%\ " System.Data.SQLite.dll /XO
 
 robocopy "%TargetDir% " "%TargetDir%Plugins\ " /XF %ProjectName%.* *.json /IS /MOV /XD Plugins Data Deps
 
