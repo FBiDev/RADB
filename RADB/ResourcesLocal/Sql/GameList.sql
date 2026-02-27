d3a22d01fafaf4400c862c4df6de65aee80dee89
FBiDev <fernando.birnfeld@gmail.com>
2026-02-27 17:54:28 -0300

    Update: database

--- RADB/ResourcesLocal/Sql/GameList.sql
+++ RADB/ResourcesLocal/Sql/GameList.sql
@@ -20,7 +20,7 @@ WHERE 1 = 1
 	AND g.ID != (CASE WHEN @allTables = 1 THEN (SELECT -1) ELSE COALESCE((SELECT ID FROM GameToPlay AS p WHERE p.ID = g.ID), -1) END) 
 	AND g.ID != (CASE WHEN @allTables = 1 THEN (SELECT -1) ELSE COALESCE((SELECT ID FROM GameToHide AS h WHERE h.ID = g.ID), -1) END) 
 	--AND c.ID <> 100 AND c.ID <> 101 
-	AND c.Active = 1 
+	--AND c.Active = 1 
 	AND ci.Active = 1 
 	AND c.IsGameSystem = 1 
 	AND (COALESCE(@ID, 0) = 0 OR g.ID = @ID) 
